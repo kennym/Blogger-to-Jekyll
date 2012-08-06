646ca9520f790695e6cf76bbc0b07e517b349070
Kenny Meyer <knny.myer@gmail.com>
2012-08-06 07:53:04 -0400

    Add to project

--- blogspot_to_jekyll.rb
+++ blogspot_to_jekyll.rb
@@ -1,33 +1,14 @@
-#!/usr/bin/env ruby
-#
-# Convert blogger (blogspot) posts to jekyll posts
-#
-# Basic Usage
-# -----------
-#
-#   ./blogger_to_jekyll.rb feed_url
-#
-#  where `feed_url` can have the following format:
-#
-#  http://{your_blog_name}.blogspot.com/feeds/posts/default
-#
-# Requirements
-# ------------
-# 
-#  * feedzirra: https://github.com/pauldix/feedzirra
-#
-# Notes
-# -----
-#
-#  * Make sure Blogger shows full output of article in feeds.
-#  * Commenting on migrated articles will be set to false by default.
-
-include Config
+include RbConfig
 
 require 'rubygems' if CONFIG['host_os'].start_with? "darwin"
-require 'feedzirra'
 require 'date'
 require 'optparse'
+begin
+  require 'feedzirra'
+rescue GEM::LoadError
+  puts "BOOM! Please install the `feedzirra` gem first."
+  return
+end
 
 def parse_post_entries(feed, verbose)
   posts = []
@@ -38,7 +19,7 @@ def parse_post_entries(feed, verbose)
     title = post.title
     file_name = creation_date.to_s + "-" + title.split(/  */).join("-").delete('\/') + ".html"
     content = post.content
-    
+
     obj["file_name"] = file_name
     obj["title"] = title
     obj["creation_datetime"] = created_datetime
@@ -56,7 +37,7 @@ def write_posts(posts, verbose)
   total = posts.length, i = 1
   posts.each do |post|
     file_name = "_posts/".concat(post["file_name"])
-    header = %{---           
+    header = %{---
 layout: post
 title: #{post["title"]}
 date: #{post["creation_datetime"]}
@@ -71,7 +52,7 @@ categories: #{post["categories"]}
       f.write(post["content"])
       f.close
     }
-    
+
     if verbose
       puts "  [#{i}/#{total[0]}] Written post #{file_name}"
       i += 1
@@ -85,14 +66,14 @@ def main
     opt.banner = "Usage: ./blogger_to_jekyll.rb FEED_URL [OPTIONS]"
     opt.separator ""
     opt.separator "Options"
-    
+
     opt.on("-v", "--verbose", "Print out all.") do
       options[:verbose] = true
     end
   end
 
   opt_parser.parse!
-  
+
   if ARGV[0]
     feed_url = ARGV.first
   else
@@ -102,10 +83,10 @@ def main
 
   puts "Fetching feed #{feed_url}..."
   feed = Feedzirra::Feed.fetch_and_parse(feed_url)
-  
+
   puts "Parsing feed..."
   posts = parse_post_entries(feed, options[:verbose])
-  
+
   puts "Writing posts to _posts/..."
   write_posts(posts, options[:verbose])
 
